01b09eb0cff99b1b8970a9143307643a898383a0
safirasn21 <safirasn021@gmai.com>
2023-05-24 11:22:12 +0700

    menambahkan fungsi main

--- paaradigmaoop/paaradigmaoop.cpp
+++ paaradigmaoop/paaradigmaoop.cpp
@@ -35,3 +35,11 @@ public:
 		return "Halo, nama saya " + nama + " dari sekolah" + sekolah + "\n\n";
 	}
 };
+
+int main() {
+	pelajar siswa1("andi laksosno", "SMA 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
